b5766c58c711dae394384ba54f98c615f6ea82d5
Robert Flight <r.flight79@gmail.com>
2014-11-18 17:27:41 -0500

    made binned_function handle when not all chromosomes match

--- R/parp1.R
+++ R/parp1.R
@@ -12,11 +12,24 @@
 binned_function <- function(bins, numvar, binfun = "mean", mcolname = "avg"){
   stopifnot(is(bins, "GRanges"))
   stopifnot(is(numvar, "RleList"))
-  stopifnot(identical(seqlevels(bins), names(numvar)))
+
+  bin_names <- seqlevels(bins)
+  numvar_names <- names(numvar)
+
+  calc_names <- intersect(bin_names, numvar_names)
+
+  stopifnot(length(calc_names) > 0)
+
+  zero_names <- setdiff(bin_names, numvar_names)
+  has_bin_names <- FALSE
+  if (length(names(bins)) != 0){
+	  has_bin_names <- TRUE
+  }
+
   
   bins_per_seqname <- split(ranges(bins), as.character(seqnames(bins)))
   
-  means_list <- mclapply(names(numvar),
+  calc_list <- mclapply(calc_names,
                          function(seqname){
                            views <- Views(numvar[[seqname]],
                                          bins_per_seqname[[seqname]])
@@ -26,7 +39,22 @@ binned_function <- function(bins, numvar, binfun = "mean", mcolname = "avg"){
                                   min = viewMins(views),
                                   max = viewMaxs(views))
                          })
-  new_mcol <- unsplit(means_list, as.factor(as.character(seqnames(bins))))
+
+  zero_list <- lapply(zero_names,
+		      function(seqname){
+			      zero_bin <- bins_per_seqname[[seqname]]
+			      n_zero_bin <- length(zero_bin)
+			      out_bin <- vector("numeric", n_zero_bin)
+			      if (has_bin_names){
+				      names(out_bin) <- names(zero_bin)
+			      }
+		      	      out_bin
+		      })
+  names(calc_list) <- calc_names
+  names(zero_list) <- zero_names
+  bin_list <- c(calc_list, zero_list)
+  bin_list <- bin_list[bin_names]
+  new_mcol <- unsplit(bin_list, as.factor(as.character(seqnames(bins))))
   mcols(bins)[[mcolname]] <- new_mcol
   return(bins)
 }
