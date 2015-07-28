27e6b46d9234404b40b37bb4a19cc0b109ad68e8
Robert Flight <r.flight79@gmail.com>
2015-07-28 15:37:22 -0400

    updated install script

--- .install_data.sh
+++ .install_data.sh
@@ -4,8 +4,8 @@ rm -rf fmdatabreastcaparp1 || exit 0;
 git clone https://github.com/rmflight/fmdatabreastcaparp1.git
 
 mkdir fmdatabreastcaparp1/data
-wget https://dl.dropbox.com/s/89xgvje4rfl0lml/fmdatabreastcaparp1.zip
-unzip fmdatabreastcaparp1.zip -d fmdatabreastcaparp1/data
+wget http://downloads.figshare.com/article/public/1266451.zip
+unzip 1266451.zip -d fmdatabreastcaparp1/data
 
 Rscript -e 'library(devtools); install("fmdatabreastcaparp1")'
 
